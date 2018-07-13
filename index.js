19e063351d7c860742dcccf58deec908379c0d96
ilanarubin <ilanarubin007@gmail.com>
2018-07-13 22:55:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseStudentNames (list) {
+ return list.map(function (student) {
+  return student.toLowerCase();
+ });
+}
+
+function nameToAttributes(list){
+  return list.map(function(driver) {
+    const firstName = driver.split(' ')[0];
+    const lastName = driver.split(' ')[1];
+    return {firstName: firstName, lastName: lastName};
+  });
+}
+function attributesToPhrase(names){
+  return names.map(function (driver){
+  return `${driver.name} is from ${driver.hometown}`
+  });
+}
